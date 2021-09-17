099ef63d4142b98f6f4db876ba8d78024b2791c8
Tahiana <tahiana.andriamb@gmail.com>
2021-09-17 19:12:50 +0300

    Update index.js

--- controller/index.js
+++ controller/index.js
@@ -6,37 +6,30 @@ app.controller("ctrl", function ctrl($scope, $http) {
     var liste = [];
 
     $scope.un1Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("un1").innerHTML);
         liste.push(document.getElementById("un1").innerHTML);
     };
 
     $scope.un2Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("un2").innerHTML);
         liste.push(document.getElementById("un2").innerHTML);
     };
 
     $scope.deux1Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("deux1").innerHTML);
         liste.push(document.getElementById("deux1").innerHTML);
     };
 
     $scope.deux2Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("deux2").innerHTML);
         liste.push(document.getElementById("deux2").innerHTML);
     };
 
     $scope.trois1Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("trois1").innerHTML);
         liste.push(document.getElementById("trois1").innerHTML);
     };
 
     $scope.trois2Fonc = function() {
-        // alert("Vous avez choisi " + document.getElementById("trois2").innerHTML);
         liste.push(document.getElementById("trois2").innerHTML);
     };
 
     $scope.valider = function() {
-        // alert("show");
         $scope.liste = liste;
         $scope.show = "Voici la liste de vos choix";
     };
